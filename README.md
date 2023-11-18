d083fb1f6e7268ac2474be37754ad15057e134f7
Nicholas Kennedy <nmk10062@gmail.com>
2023-11-17 22:41:06 -0500

    Remove Poppins font, Add image assets for hero, Add subheader to hero

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # musikalis
+
 A clone of the design from https://dribbble.com/shots/21100671-Musikalis-Music-Platform-Website
+
+## Image Credits
+
+- Image by [Freepik](https://www.freepik.com/free-photo/low-angle-female-dj-club_5795392.htm#page=2&position=9&from_view=collections)
+- Image by [KamranAydinov](https://www.freepik.com/free-photo/black-standing-micropfone-studio_7608080.htm#query=microphone%20clip%20art&position=0&from_view=keyword&track=ais) on Freepik
--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ function App() {
   return (
     <div>
       <Header />
+
+      <div className="App__hero">
+        <div className="App__header-text">
+          <h1>Music Is The Shorthand</h1>
+          <h1 className="App__header-text-low-opacity">Of Emotion</h1>
+        </div>
+        <div>
+          <p className="text-large App__subheader-text">
+            It's our mission at RouteNote to give you the opportunities <br />
+            to take your music as far as you want it to go.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }

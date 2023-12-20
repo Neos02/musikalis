5d3fc1fc3828f0ae82e62da54e72ac0afb91e67c
Nicholas Kennedy <nmk10062@gmail.com>
2023-12-20 13:50:40 -0500

    Fix header responsiveness

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Button from "../Button/Button";
 import { green } from "../../colors";
 import "./Header.css";
+import { faBars, faXmark } from "@fortawesome/free-solid-svg-icons";
 
 const Header = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <header className="Header__header">
       <a href="#">
         <h3>Musikalis</h3>
       </a>
 
-      <nav className="Header__nav">
+      <nav className={`Header__nav ${menuOpen ? "Header__open" : ""}`}>
         <ul>
           <li>
             <a href="#">Home</a>
@@ -28,10 +31,18 @@ const Header = () => {
         </ul>
       </nav>
 
-      <div className="Header__auth-btns">
+      <div className={`Header__auth-btns ${menuOpen ? "Header__open" : ""}`}>
         <a href="#">Sign in</a>
         <Button label="Sign Up" backgroundColor={`rgb(${green})`} />
       </div>
+
+      <Button
+        className="Header__toggle-btn"
+        icon={menuOpen ? faXmark : faBars}
+        backgroundColor="transparent"
+        textColor="white"
+        onClick={() => setMenuOpen((prevMenuOpen) => !prevMenuOpen)}
+      />
     </header>
   );
 };
